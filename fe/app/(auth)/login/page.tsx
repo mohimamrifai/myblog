b0e5338a9140e8ecd38e91df17fae51c32f1353f
mohimamrifai <imamrifaibisnis@gmail.com>
2024-05-01 14:22:15 +0700

    feat (fe): login page

--- fe/app/(auth)/login/page.tsx
+++ fe/app/(auth)/login/page.tsx
@@ -1,12 +1,14 @@
+'use client'
 import Field from "@/components/Field";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import React from "react";
+import { Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 export default function Page() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <Card className="w-3/12">
@@ -14,18 +16,38 @@ export default function Page() {
           <CardTitle className="text-center">Login</CardTitle>
         </CardHeader>
         <CardContent>
-          <Field label="Email" type="email" placeholder="Email" htmlFor="email" className="mb-3"  />
-          <Field label="Password" type="password" placeholder="Password" htmlFor="password" className="mb-3"  />
-          <div className="flex items-center space-x-2 mb-5">
-            <Checkbox id="remember" />
-            <label
-              htmlFor="remember"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Remember Me
-            </label>
+          <Field
+            label="Email"
+            type="email"
+            placeholder="Email"
+            htmlFor="email"
+            className="mb-3"
+          />
+          <Field
+            label="Password"
+            type={isPasswordVisible ? "text" : "password"}
+            placeholder="Password"
+            htmlFor="password"
+            className="mb-3"
+          />
+          <div className="flex justify-between items-center mb-5">
+            <div className="flex items-center space-x-2 ">
+              <Checkbox id="remember" />
+              <label
+                htmlFor="remember"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Remember Me
+              </label>
+            </div>
+            {isPasswordVisible ? 
+            <Eye size={18} className="cursor-pointer" onClick={() => setIsPasswordVisible(!isPasswordVisible)} /> : 
+            <EyeOff size={18} className="cursor-pointer" onClick={() => setIsPasswordVisible(!isPasswordVisible)} />}
           </div>
           <Button className="w-full">Login</Button>
+          <p className="text-center mt-2 text-sm">don't have an account yet ? 
+            <Link href="/register" className="underline text-blue-400"> Register</Link>
+          </p>
         </CardContent>
       </Card>
     </div>
