80d9d668c308397afa4b2089b1cf7ca289133f4e
mohimamrifai <imamrifaibisnis@gmail.com>
2024-05-02 13:42:48 +0700

    fixing

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors'
 import postsRoutes from './routes/postsRoutes'
 import categoriesRoutes from './routes/categoriesRoutes'
+import authRoutes from './routes/authRoutes'
 
 const app = express();
 const port = 8000;
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/v1/thumbnail', express.static('src/assets/thumbnail'))
 app.use('/api/v1/posts', postsRoutes);
 app.use('/api/v1/categories', categoriesRoutes);
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
