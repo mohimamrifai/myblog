a49e9a74e97e920f66768d48eb379e64d9436083
mohimamrifai <imamrifaibisnis@gmail.com>
2024-05-04 21:52:02 +0700

    fix (be): fixing the routes

--- be/src/routes/categoriesRoutes.ts
+++ be/src/routes/categoriesRoutes.ts
@@ -3,11 +3,13 @@ import express, { Router } from "express";
 import { allCategories } from "../controllers/category/allCategories";
 import { createCategory } from "../controllers/category/createCategory";
 import { categoryById } from "../controllers/category/categoryById";
+import { deleteCategory } from "../controllers/category/deleteCategory";
 
 const router: Router = express.Router();
 
 router.get("/", allCategories);
 router.get("/:id", categoryById);
 router.post("/", createCategory);
+router.delete("/:id", deleteCategory);
 
 export default router;
