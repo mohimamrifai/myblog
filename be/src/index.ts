449706ec8dc5ae4b9f2e52d58eb5b55a6d5b0651
mohimamrifai <imamrifaibisnis@gmail.com>
2024-05-05 13:43:04 +0700

    fix (be): modified the index.ts

--- be/src/index.ts
+++ be/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import postsRoutes from './routes/postsRoutes'
 import categoriesRoutes from './routes/categoriesRoutes'
 import authRoutes from './routes/authRoutes'
+import userRoutes from './routes/userRoutes'
 
 const app = express();
 const port = 8000;
@@ -15,6 +16,7 @@ app.use('/api/v1/avatar', express.static('src/assets/avatar'))
 app.use('/api/v1/posts', postsRoutes);
 app.use('/api/v1/categories', categoriesRoutes);
 app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/profile', userRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
