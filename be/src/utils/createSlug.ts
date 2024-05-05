88848058cda29a93106d427e30b67721d53f1fff
mohimamrifai <imamrifaibisnis@gmail.com>
2024-05-05 14:44:34 +0700

    refac (be): refactor create slug function

--- be/src/utils/createSlug.ts
+++ be/src/utils/createSlug.ts
@@ -1,3 +1,8 @@
-export function createSlug (title: string) {
-    return title.toLowerCase().split(" ").join("-")
+export function createSlug(title: string): string {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, "") // Removes characters that are not alphanumeric, space, or dash
+    .replace(/\s+/g, "-") // Replace spaces with a dash
+    .replace(/-+/g, "-") // Replace consecutive dashes with a single dash
+    .replace(/^-|-$/g, ""); // Remove leading and trailing dashes
 }
